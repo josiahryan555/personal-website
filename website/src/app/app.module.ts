e32db7751033447fd0e52df3a5b8acc37538d20f
jryanb0b <jryanbob@gmail.com>
2022-11-29 18:45:06 -0500

    Added Angular project and page components

---
+++ website/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PortfolioPageComponent,
+    AboutPageComponent,
+    ContactPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
