61022d4124565b985b34da0a33debe34f72dd421
jryanb0b <jryanbob@gmail.com>
2022-12-05 11:21:57 -0500

    added page stuff

--- website/src/app/app.component.spec.ts
+++ website/src/app/app.component.spec.ts
@@ -1,3 +1,4 @@
+import { NgStyle } from '@angular/common';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
@@ -6,7 +7,8 @@ describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        NgStyle
       ],
       declarations: [
         AppComponent
--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+// import { NgOptimizedImage } from '@angular/common'
+// import './codePic.png';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'website';
+  backgroundImg = "../assests/codePic.png";
 }
