ddd0ff57300626985f9f59eb891536583dc04eb3
Clare Schellenberg <cas63@calvin.edu>
2022-12-13 16:14:08 -0500

    final edits

--- website/src/app/app-routing.module.ts
+++ website/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { AppComponent } from './app.component';
-import { ContactFormComponent } from './components/contact-form/contact-form.component';
+// import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { ProjInfoPageComponent } from './components/proj-info-page/proj-info-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 import { PortfolioPageComponent } from './portfolio-page/portfolio-page.component';
@@ -14,9 +14,8 @@ const routes: Routes = [
   { path: 'contact', component: ContactPageComponent },
   { path: 'portfolio', component: PortfolioPageComponent },
   { path: 'proj-info/:id', component: ProjInfoPageComponent },
-  { path: 'contact-form', component: ContactFormComponent },
+  // { path: 'contact-form', component: ContactFormComponent },
   { path: 'landing-page', component: LandingPageComponent },
-
 
   // otherwise redirect to home
   { path: '**', redirectTo: '' },
--- website/src/app/components/proj-info-page/proj-info-page.component.ts
+++ website/src/app/components/proj-info-page/proj-info-page.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { DataService, PortfolioInfo} from 'src/app/data.service';
-
-
+import { DataService, PortfolioInfo } from 'src/app/data.service';
 @Component({
   selector: 'app-proj-info-page',
   templateUrl: './proj-info-page.component.html',
@@ -12,27 +10,20 @@ export class ProjInfoPageComponent implements OnInit {
   @Input() detailImg1: string = "";   /// remove
 
   public id: number = -1;
-  public project?: PortfolioInfo 
-
-  // follow slide 39 week9
+  public project?: PortfolioInfo
 
   constructor(
     private actRt: ActivatedRoute,
     private data_s: DataService
   ) { }
 
-
   ngOnInit(): void {
     this.id = Number(this.actRt.snapshot.paramMap.get('id'));
     console.log(' ', this.id);
-    
+
     this.project = this.data_s.getPortfolioInfo()[this.id]
     //console.log(this.data_s.getPortfolioInfo()[this.id]);
-    
-    
-    
-    
+
     // call dataservice to get the info for this project based on the id.
   }
-
 }
